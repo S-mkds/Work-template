cc0ead0cdc1e67e08eca5499453aefde68085798
John Doe <Mokaddem-s@hotmail.com>
2022-07-29 18:10:45 +0200

    more project

---
+++ Projet 5 Curseur stylisé/app.js
@@ -0,0 +1,22 @@
+const curseur = document.querySelector('.curseur');
+
+document.addEventListener('mousemove', e => {
+
+    curseur.style.top = e.pageY - 20 + "px";
+    curseur.style.left = e.pageX - 20 + "px";
+    // console.log(e.pageY, e.pageX);
+
+})
+
+document.addEventListener('click', () => {
+
+    curseur.classList.add('expand');
+
+    setTimeout(() => {
+        curseur.classList.remove('expand');
+    }, 500)
+
+})
+
+
+
